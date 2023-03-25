9116fde22b178a70e94986eeb1687db1ce5f6e6b
luyosamson <luyosamson@gmail.com>
2023-03-25 23:08:53 +0300

    Import a module

--- Variable.py
+++ Variable.py
@@ -2,3 +2,10 @@ site_name="programiz.com"
 print(site_name)
 site_name="Samson.com"
 print(site_name)
+
+#Importing a module
+import  Constant
+
+print(Constant.PI*Constant.Radius*Constant.Radius)
+
+
