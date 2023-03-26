2326d63f3125dfdb682c39ef4277f1b21853cce9
luyosamson <luyosamson@gmail.com>
2023-03-26 22:28:11 +0300

    List datatype

--- Datatype.py
+++ Datatype.py
@@ -1,2 +1,8 @@
 num1=1+2j
 print(num1, "is of type", type(num1))
+
+#List datatype
+#List are ordered,and stoted in [] bracket
+
+languages=["Python","JavaScript","Java","C#","C","C++"]
+print(languages[4])
