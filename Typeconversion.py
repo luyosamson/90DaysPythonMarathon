7f0ca6058dc6f8d152504e413c0821eb802face3
luyosamson <luyosamson@gmail.com>
2023-03-27 22:47:59 +0300

    Type conversion

---
+++ Typeconversion.py
@@ -0,0 +1,10 @@
+num_str='67'
+num_int=45
+print("Data type before type casting",type(num_str))
+
+num_str=int(num_str)
+
+print("Datatype after conversion",num_str)
+
+num_sum=num_str+num_int
+print("Sum is",num_sum)
