aa1ee069db2551fef1de8485489ba05f4f55d39b
luyosamson <luyosamson@gmail.com>
2023-03-28 14:31:19 +0300

    Operators in python

---
+++ Operators.py
@@ -0,0 +1,17 @@
+a=7
+b=10
+
+# addition
+# subtraction
+# multiplication
+# division
+# floor division
+# modulo
+# a to the power b
+
+print("Addition",a+b)
+print("Substraction",b-a)
+print("Multiplication",a*b)
+print("Division",b/a)
+print("Modulo",10%7)
+print("Power",7**10)
