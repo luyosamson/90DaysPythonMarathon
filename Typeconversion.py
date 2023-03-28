26eafe67d5b3e282f694e709b36f6cc400136719
luyosamson <luyosamson@gmail.com>
2023-03-28 14:03:06 +0300

    Type converted user input

--- Typeconversion.py
+++ Typeconversion.py
@@ -1,13 +1,13 @@
-num_str='67'
-num_int=45
-print("Data type before type casting",type(num_str))
+# num_str='67'
+# num_int=45
+# print("Data type before type casting",type(num_str))
 
-num_str=int(num_str)
+# num_str=int(num_str)
 
-print("Datatype after conversion",num_str)
+# print("Datatype after conversion",num_str)
 
-num_sum=num_str+num_int
-print("Sum is",num_sum)
+# num_sum=num_str+num_int
+# print("Sum is",num_sum)
 
 
 #I/O in Python
@@ -16,17 +16,17 @@ print("Sum is",num_sum)
 x=67
 y=90
 
-print("The value of y is {} and the value of x is {}".format(y,x))
+# print("The value of y is {} and the value of x is {}".format(y,x))
 
-num =input("Enter your age\n")
-name=input("Enter youe Full name\n")
+# num =input("Enter your age\n")
+# name=input("Enter youe Full name\n")
 
-print('{} is {} years old'.format(name,num))
+# print('{} is {} years old'.format(name,num))
 
 #Converting user input to appropriate data type
 
-x=input("Enter numb1")
-y=input("Enter numb2")
-sum=x+y
+x=input("Enter numb1\t")
+y=input("Enter numb2\t")
+sum=int(x)+int(y)
 
 print("The sum is",sum)
