a1e5718f11eba0e1aeaf7050417bc684d229a55e
luyosamson <luyosamson@gmail.com>
2023-03-29 23:13:15 +0300

    Added all files

---
+++ Conditional/Excercise4.py
@@ -0,0 +1,3 @@
+# Write a program that takes a list of integers as input 
+# and checks if all the numbers in the list are positive.
+ 
--- Conditional/Exercise3.py
+++ Conditional/Exercise3.py
@@ -1,2 +1,11 @@
-# Write a program that takes a string 
-# as input and checks if it is a palindrome.
+# Write a program that takes an integer 
+# as input and checks if it is even or odd.
+
+x=int(input("Enter an integer\n"))
+
+if x%2==0:
+    print("The number is an even number")
+else:
+    print("The number is an odd number")
+ 
+ 
