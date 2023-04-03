d5cb915f1c683e57529254b2ea8866f7d0ef1291
luyosamson <luyosamson@gmail.com>
2023-04-03 09:02:55 +0300

    Created a datatype file

---
+++ Datatype/Lists.py
@@ -0,0 +1,10 @@
+# Lists are ordered
+# It can store various types of elements
+# Elements can be accessed by index
+# Are mutable-Changeable
+# List allows duplicate elements
+
+students = ['Samson', 'James', 'Emmanule', 'Grace', 'Phelix', 'Omondi']
+print(students)
+students2=['Mary','Otieno',[4,6,8,10],'Victor']
+print(students2[2])
