980fe761db976127013f88d2205a736fe20bf411
luyosamson <luyosamson@gmail.com>
2023-04-03 11:41:19 +0300

    Tuples method

--- Datatype/Tuples.py
+++ Datatype/Tuples.py
@@ -7,3 +7,16 @@ print(fruits)
 print(fruits[-2])
 #Repeat
 print(('Apple',)*4)
+
+#In appending an item to a tuple,you first change a tuple to a list
+
+# y=list(fruits)
+# y.append("Grapes")
+# print(tuple(y))
+
+fruits2=('Avocado',)
+fruits+=fruits2
+print(fruits)
+
+for n in  fruits:
+    print(n)
