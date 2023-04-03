94dc44c3b2c66219044bb9a1d64fb7ed5d71c0a6
luyosamson <luyosamson@gmail.com>
2023-04-03 12:20:15 +0300

    Used various Dictionaries methos

--- Datatype/Dictionaries.py
+++ Datatype/Dictionaries.py
@@ -8,3 +8,25 @@
         #Elements cannot be accessed by index
         #Are mutable-Can change or remove item after dict have been created
         #Does not allow duplications
+lang={1:'Java',2:'JavaScript',3:'Python', 4:'Ruby',5:'Scala',6:'C++',7:'Nodejs'}
+
+print(lang)
+
+print(lang[4])
+print(lang.get(3))
+print(lang.pop(5))
+print(lang)
+print(len(lang))
+print(sorted(lang))
+
+print(lang.values())
+lang.update({5:'Pascal'})
+print(lang)
+lang.popitem()
+print(lang)
+
+for a,b in lang.items():
+    print(a,b)
+
+lang2=lang.copy()
+print(lang2)
