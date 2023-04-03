89d2f45c9cc5d899058f475648d503fda371bd3b
luyosamson <luyosamson@gmail.com>
2023-04-03 11:54:12 +0300

    Short notes on Dictionaries

---
+++ Datatype/Dictionaries.py
@@ -0,0 +1,10 @@
+#It is a collection of values,items or obejects of different types stored in 
+#key vale pairs enclosed in curly  braces and each key is  separated from its
+# value by a colon
+
+#Characteristics
+        #Are ordered in python 3.7
+        #Can store various types of elements
+        #Elements cannot be accessed by index
+        #Are mutable-Can change or remove item after dict have been created
+        #Does not allow duplications
