3b9963a2d3016190e06d4e6a25ff7d688df314b8
luyosamson <luyosamson@gmail.com>
2023-04-04 11:48:16 +0300

    Created Set file

---
+++ Datatype/Sets.py
@@ -0,0 +1,8 @@
+#Items are not in a particular orders
+#No duplicate items are allowed
+#Items must be imuutable objects
+
+
+#Set remove duplicate items,so cat will only appear onces
+animals={'Dog','Elephant','Tiger','Pig','Cat','Zebra','Cat'}
+print(animals)
