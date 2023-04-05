b7d9689e03e80dd39449ea74ed5a01ac57f122bb
luyosamson <luyosamson@gmail.com>
2023-04-05 23:11:50 +0300

    Modified

--- Functions/Square.py
+++ Functions/Square.py
@@ -2,7 +2,7 @@
 
 
 def find_sqr(num):
-    num=int(input("Enter the number to find it's square\t"))
+    # num=int(input("Enter the number to find it's square\t"))
     sq=num*num
     return sq
 
@@ -19,3 +19,7 @@ power=pow(4,2)
 print("The power",power)
 sqr_root=math.sqrt(49)
 print(sqr_root)
+
+for i in range(1,10):
+    sqr=find_sqr(i)
+    print("The square of",i ,"=",sqr)
