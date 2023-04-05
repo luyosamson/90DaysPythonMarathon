ea35d71429a5c96246191d0f6a27238192fd6db9
luyosamson <luyosamson@gmail.com>
2023-04-05 22:51:02 +0300

    Working with build in functions

---
+++ Functions/Square.py
@@ -0,0 +1,21 @@
+
+
+
+def find_sqr(num):
+    num=int(input("Enter the number to find it's square\t"))
+    sq=num*num
+    return sq
+
+square=find_sqr(5)
+
+print("Square",square)
+
+mult=square*2
+print("Twice the square",mult)
+
+
+import math
+power=pow(4,2)
+print("The power",power)
+sqr_root=math.sqrt(49)
+print(sqr_root)
