77877f829943eb1914bce15c87bf6ae9bbda2ec0
luyosamson <luyosamson@gmail.com>
2023-04-15 22:41:12 +0300

    Wrote a lambda function to get square of numbers in a list

--- Functions/Lambdafxn.py
+++ Functions/Lambdafxn.py
@@ -1,4 +1,17 @@
 #Lambda fxn is a sinle line functions
 
-add=lambda n1,n2 : n1+n2
-print(add(12,80))
+# add=lambda n1,n2 : n1+n2
+# print(add(12,80))
+
+
+#Getting square of numbers
+numbers=[4,2,6,89,45]
+
+sqr_numb=[]
+
+sqr=lambda x: x**2
+
+for x in numbers:
+    sqr_numb.append(sqr(x))
+
+print(sqr_numb)
