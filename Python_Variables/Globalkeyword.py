3e18c0643d60a1c99bfc54c26f873a5e7a0cb980
luyosamson <luyosamson@gmail.com>
2023-05-01 19:16:56 +0300

    Access and modified global variable

--- Python_Variables/Globalkeyword.py
+++ Python_Variables/Globalkeyword.py
@@ -1,11 +1,17 @@
+
+
+        # Access and Modify Python Global Variable
 c=10
 def x():
  
  #Use global keyword to modify the global variable inside a function
  global c
+#  c=10
 
  #Increament c
  c=c+2
  print(c)
 
 x()
+
+            # Global in Nested Functions
