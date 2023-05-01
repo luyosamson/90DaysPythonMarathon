c07d08e275ecf3907979bb780c2d5ed0619f5649
luyosamson <luyosamson@gmail.com>
2023-05-01 22:06:34 +0300

    Used global key word

--- Python_Variables/Globalkeyword.py
+++ Python_Variables/Globalkeyword.py
@@ -1,17 +1,34 @@
 
 
-        # Access and Modify Python Global Variable
-c=10
-def x():
+#         # Access and Modify Python Global Variable
+# c=10
+# def x():
  
- #Use global keyword to modify the global variable inside a function
- global c
-#  c=10
+#  #Use global keyword to modify the global variable inside a function
+#  global c
+# #  c=10
 
- #Increament c
- c=c+2
- print(c)
+#  #Increament c
+#  c=c+2
+#  print(c)
 
-x()
+# x()
 
             # Global in Nested Functions
+def outer_fnx():
+    x=100
+
+    def inner_fxn():
+      global x
+      x=200
+    print("Before calling inner function",x)
+    inner_fxn()
+
+    print("After calling inner function",x)
+ 
+outer_fnx()
+
+print("After calling outer function",x)
+
+
+
