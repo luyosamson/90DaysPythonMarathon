a103f03db8c153e7a8e1ca5003b9de54fc0825b9
luyosamson <luyosamson@gmail.com>
2023-05-02 11:26:31 +0300

    Added another Folder and introductory note

---
+++ Package/Introduction.py
@@ -0,0 +1,7 @@
+# While working on big projects, we have to deal with a 
+# large amount of code, and writing everything together in the
+# same file will make our code look messy. Instead, we can separate 
+# our code into multiple files by keeping the related code together in packages.
+
+# Now, we can use the package whenever we need it in our projects. 
+# This way we can also reuse our code.
