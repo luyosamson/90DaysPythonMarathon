b91de74bc00de63e55fdc3aa062f6955d57cd890
luyosamson <luyosamson@gmail.com>
2023-05-05 11:34:37 +0300

    Create a challeneg

---
+++ Codding Challenges/Quiz.py
@@ -0,0 +1,38 @@
+# Challenge: Implement a student grading system 
+# that consists of three modules: a main module, a grading module, and a 
+# student module. The main module should use the grading and student modules 
+# to calculate the final grades for a group of students.
+
+
+# The grading module should contain functions to calculate the grades for 
+# individual assignments and for the overall course grade. The student module 
+# should contain a class definition for a Student object, which stores the student's name, 
+# ID, and assignment grades.
+
+# Here are the requirements for the grading system:
+
+# # The grading module should have a function that takes a 
+# list of grades as input and returns the average grade.
+
+# The grading module should have a function that takes a list of grades and a
+#  weight for each assignment, and returns the weighted average grade.
+
+# The grading module should have a function that takes a list of weighted 
+# grades and their corresponding weights, and returns the overall course grade.
+
+# The student module should define a Student class that stores the student's name, ID, 
+# and assignment grades.
+
+# The Student class should have methods to add an assignment grade and to 
+# calculate the overall course grade.
+
+# The main module should create a list of Student objects, 
+# add assignment grades for each student, and then calculate the overall course grades 
+# using the grading module.
+
+# To complete this challenge, you will need to create three separate Python files: 
+# one for the main module, one for the grading module, and one for the student module. 
+# You will also need to use the import statement to import the necessary modules and 
+# functions/classes.
+
+# Good luck!
