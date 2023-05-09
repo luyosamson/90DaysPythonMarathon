dd99d8c133cc350b7f08fbc8f678bf6b9d50416f
luyosamson <luyosamson@gmail.com>
2023-05-09 12:41:59 +0300

    Modified

---
+++ Directory/changedir.py
@@ -0,0 +1,11 @@
+# In Python, we can change the current working directory 
+# by using the chdir() method.
+
+# The new path that we want to change into must be supplied as a
+# string to this method. And we can use both the forward-slash / 
+# or the backward-slash \ to separate the path elements.
+
+
+import os
+os.chdir('/home/pyramid/Development')
+print(os.getcwd())
---
+++ Directory/dir.py
@@ -0,0 +1,7 @@
+# Get Current Directory in Python
+# We can get the present working directory using the getcwd() method of the os module.
+
+# This method returns the current working directory in the form of a string. For example,
+
+import os
+print(os.getcwd())
