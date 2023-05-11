950dfe89d1868a40e8cbe177369a706e5d068b22
luyosamson <luyosamson@gmail.com>
2023-05-11 09:24:50 +0300

    Added TryFinally

---
+++ ExceptionsInPython/TryFinally.py
@@ -0,0 +1,4 @@
+# Python try ...finally
+# In Python, the finally block is always executed no matter whether there is an exception or not .
+
+# The finally block is optional. And, for each try block, there can be only one finally block.
