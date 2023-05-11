d99bc5476dbe8d077ebe5bc692d82e7a59c10af5
luyosamson <luyosamson@gmail.com>
2023-05-11 10:05:30 +0300

    User defined function

--- ExceptionsInPython/TryFinally.py
+++ ExceptionsInPython/TryFinally.py
@@ -2,3 +2,14 @@
 # In Python, the finally block is always executed no matter whether there is an exception or not .
 
 # The finally block is optional. And, for each try block, there can be only one finally block.
+try:
+    denominator=0
+    numerator=10
+    result=numerator/denominator
+    print(result)
+
+except:
+    print("ERROR:Denominator cannot be 0")
+
+finally:
+    print("This is the final block")
