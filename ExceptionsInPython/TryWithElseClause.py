cca780965a09fd9b09c472e181af9844fd6e4ba9
luyosamson <luyosamson@gmail.com>
2023-05-11 08:44:57 +0300

    Reciprical of even number

---
+++ ExceptionsInPython/TryWithElseClause.py
@@ -0,0 +1,16 @@
+    # Python try with else clause
+# In some situations, we might want to run a certain
+# block of code if the code block inside try runs without any errors.
+
+# For these cases, you can use the optional else keyword with the try statement.
+
+
+#Program to print reciprical of even numbers
+
+num=int(input("Enter a number "))
+
+if num%2!=0:
+    print("Not an even number")
+else:
+    reciprical=1/num
+    print("Reciprical of " +str(num)+ " is",str(reciprical))
