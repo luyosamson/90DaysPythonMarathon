32f67bf01820660fde1e1084d1b1701db78241f4
luyosamson <luyosamson@gmail.com>
2023-05-17 14:49:52 +0300

    Created a file to calculate area

---
+++ OOP/area.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    length=0
+    width=0
+
+    #Method to calculate area
+    def area(self):
+        result=self.length*self.width
+        print("Area is ",result)
+
+rectangle=Rectangle()
+rectangle.length=20
+rectangle.width=20
+rectangle.area()
