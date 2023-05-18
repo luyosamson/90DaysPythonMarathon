2e6430c103f9475df31fb4328293c1ec8051d4b5
luyosamson <luyosamson@gmail.com>
2023-05-18 10:01:44 +0300

    Folder for inheritace

---
+++ OOP/Inheritance/animals.py
@@ -0,0 +1,11 @@
+class Animals:
+    def eat(self):
+        print("I can eat")
+class Dog(Animals):
+    def bark(self):
+        print("I can bark")
+dog1=Dog()
+dog1.bark()
+dog1.eat()
+
+        
