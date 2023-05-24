3170ac68e65cb6c77a6b99de937309ebff669ad1
luyosamson <luyosamson@gmail.com>
2023-05-24 15:27:52 +0300

    Modified

--- Functions/fibonacci.py
+++ Functions/fibonacci.py
@@ -1,14 +1,19 @@
-def ask_ok(prompt, retries=4, reminder='Please try again!'):
-    while True:
-        ok = input(prompt)
-        if ok in ('y', 'ye', 'yes'):
-            return True
-        if ok in ('n', 'no', 'nop', 'nope'):
-            return False
-        retries = retries - 1
-        if retries < 0:
-            raise ValueError('invalid user response')
-        print(reminder)
+def fibonacci(n):
+    a,b=0,1
+    print("Fibonacci series:")
+    print(a)
 
+    if n>1:
+        print(b)
+    
+    #Generate the subsequent numbers in series
 
-ask_ok('OK to overwrite the file?', 2, 'Come on, only yes or no!')
+    for _ in range(1,n):
+        next_number=a+b
+        print(next_number)
+        # a,b=b,next_number
+        temp=a #Not going to be used
+        a=b
+        b=next_number
+         
+fibonacci(5)
