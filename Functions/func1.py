d201f0a77bb1a5fe1ff50ea468438fcbc3f5a7fb
luyosamson <luyosamson@gmail.com>
2023-05-26 10:36:03 +0300

    More files

---
+++ Functions/func1.py
@@ -0,0 +1,5 @@
+def greet_me(name,greeting):
+    print(f"{greeting} {name}")
+
+greet_me('Omos',"Hi")
+greet_me(name="Luyo",greeting="Hello")
---
+++ Functions/func2.py
@@ -0,0 +1,4 @@
+def sum(num1,num2):
+    return num1+num2
+result=sum(10,8)
+print(result)
