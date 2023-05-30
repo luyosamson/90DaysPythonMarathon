1168dde5d5541b91419a1662b7d63e40e4c6bb3a
luyosamson <luyosamson@gmail.com>
2023-05-30 10:17:23 +0300

    Modified

--- Datatype/List2.py
+++ Datatype/List2.py
@@ -3,3 +3,11 @@ l=len(furniture)
 print(l)
 furniture[2]='desk'
 print(furniture)
+numbers=['1','2','4','6','8']
+
+myList=numbers+furniture
+print(myList)
+
+for index,item in enumerate(myList):
+    print(f"Index :{index}-item:{item}")
+    
