49f1b2127f16faf44b62ae1d74b419af3de1521c
luyosamson <luyosamson@gmail.com>
2023-05-31 00:09:52 +0300

    Modified

--- Datatype/Dict1.py
+++ Datatype/Dict1.py
@@ -6,17 +6,23 @@ student={
     "sex":"Male"
 }
 
-student['Marital_Status']="Single"
+# student['Marital_Status']="Single"
+# print(student)
+
+# for value in student.values():
+#     print(value)
+
+# for ke in student:
+#     print(ke)
+
+# for ite in student.items():
+#     print(ite)
+
+# for key,value in student.items():
+#     print(f"{key}:{value}")
+
+print(f"My name is {student.get('name')},I am a student from {student.get('school')}")
+student.setdefault("Height",56)
 print(student)
-
-for value in student.values():
-    print(value)
-
-for ke in student:
-    print(ke)
-
-for ite in student.items():
-    print(ite)
-
-for key,value in student.items():
-    print(f"{key}:{value}")
+student.pop("name")
+print(student)
