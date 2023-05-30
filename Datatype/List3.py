a45646518de66b6a0890e19c2fa0a18ef20fdd81
luyosamson <luyosamson@gmail.com>
2023-05-30 16:15:13 +0300

    Modified

--- Datatype/List3.py
+++ Datatype/List3.py
@@ -1,8 +1,14 @@
-furniture=['Desk','Table','Chair','Shelf']
+furniture='Desk','Table','Chair','Shelf'
 price=[1000,600,900,1200]
+name=['Samson','Omondi','Luyo','Kevin']
 
-for item,cost in zip(furniture,price):
-    print(f'The {item} cost is ${cost}')
+for item,owner,cost in zip(furniture,name,price):
+    print(f'The {item},belongs to {owner} is costing ${cost}')
 
+print(furniture)
 
+a,b='samson','jacky'
+a,b=b,a
+print(b)
 
+furniture.index('Chair')
