d841b955e63e5d7ef156e8198a9c3564772e3b74
luyosamson <luyosamson@gmail.com>
2023-07-01 05:41:24 +0300

    created a new file

---
+++ OOP/studentTest.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self,name,age,grade):
+        self.name=name
+        self.age=age
+        self.grade=grade
+    def average_grade(self,grade):
+        grade_sum=sum(grade)
+        average_grade=grade_sum/len(grade)
+        return average_grade
+    def update_age(self,new_age):
+        self.age=new_age
+        return new_age
+    def display_information(self):
+        print("Student name",self.name)
+        print("Student age",self.age)
+        print("Student grade",self.grade)
+
+student1=Student("Samson",18,"A")
+student1.display_information()
