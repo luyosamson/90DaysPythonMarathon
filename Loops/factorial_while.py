73690f7b6ec255305633074ea8723303939905f0
Luyo Samson <luyosamson@gmail.com>
2024-05-30 20:16:47 +0300

    factorial

--- Loops/factorial_while.py
+++ Loops/factorial_while.py
@@ -4,14 +4,7 @@ i = 1
 
 
 while i <= num:
-    factorial *= 1 
+    factorial *= i
     i += 1
 print(factorial)
 
-num = 5
-factorial = 1
-i = 1
-while i <= num:
-    factorial *= i
-    i += 1
-print("Factorial:", factorial)
