46c1b8076dcda2a7d1b3b4a1f2ccd29c64c86ea2
Luyo Samson <luyosamson@gmail.com>
2024-06-26 23:53:37 +0300

    updated

--- OOP/Inheritance/MultipleInheritance/mammals.py
+++ OOP/Inheritance/MultipleInheritance/mammals.py
@@ -6,10 +6,11 @@ class Wingedmammals:
     def winged_info(self):
         print("Winged mammals can fly and flap")
 
+
+
 class Bats(Mammals,Wingedmammals):
     pass
 #Object of the Bat class
 b1=Bats()
 b1.winged_info()
 b1.mam_info()
-    
